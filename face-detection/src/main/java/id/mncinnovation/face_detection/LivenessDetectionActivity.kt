64f01849f5a9b69b9ce552fe6eb2acaf46c6554f
bagus andre wijaya <bagusandrewijaya19@gmail.com>
2023-10-24 16:02:00 +0700

    change icon

--- face-detection/src/main/java/id/mncinnovation/face_detection/LivenessDetectionActivity.kt
+++ face-detection/src/main/java/id/mncinnovation/face_detection/LivenessDetectionActivity.kt
@@ -208,6 +208,6 @@ class LivenessDetectionActivity : BaseCameraActivity(), LivenessDetectionListene
 
     companion object {
         private const val TAG = "Liveness Detection"
-        private const val COUNTDOWN_TIME = 20
+        private const val COUNTDOWN_TIME = 30
     }
 }
